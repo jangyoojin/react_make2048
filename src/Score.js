402df05b31f16e2b3c85ff3bb1911ad4d6a5a5a5
jangyoojin <candyyoojin@gmail.com>
2021-08-26 16:01:47 +0900

    Scorejs change

--- src/Score.js
+++ src/Score.js
@@ -4,15 +4,17 @@ class Score extends Component {
     constructor(props) {	
 		super(props);
 	}
-
+	
 	render() {
 		return (
 			<div className="TitleScore">
                 <span class="veryj">
                     <span class="text-style-1">2048</span>ver.yj
                 </span>
-				<span class="scorebox">Score</span>
-				<span class="bestbox">Best</span>
+				<span>
+					<span class="scorebox">Score</span>
+					<span class="bestbox">Best</span>
+				</span>
             </div>
 		);
 	}
