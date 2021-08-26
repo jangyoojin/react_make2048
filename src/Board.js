af1c824e64d1a6a81210b70525563d97de579f36
jangyoojin <candyyoojin@gmail.com>
2021-08-26 13:15:03 +0900

    finish UI

---
+++ src/Board.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+class Board extends Component {
+    constructor(props) {	
+		super(props);
+	}
+    
+	render() {
+		return (
+            <div className="background">
+			    <table id='board'>
+                    <tr>
+                        <td id="00"></td>
+                        <td id="01"></td>
+                        <td id="02"></td>
+                        <td id="03"></td>
+                    </tr>
+                    <tr>
+                        <td id="10"></td>
+                        <td id="11"></td>
+                        <td id="12"></td>
+                        <td id="13"></td>
+                    </tr>
+                    <tr>
+                        <td id="20"></td>
+                        <td id="21"></td>
+                        <td id="22"></td>
+                        <td id="23"></td>
+                    </tr>
+                    <tr>
+                        <td id="30"></td>
+                        <td id="31"></td>
+                        <td id="32"></td>
+                        <td id="33"></td>
+                    </tr>
+            </table>
+            </div>
+		);
+	}
+}
+
+
+export default Board;
---
+++ src/Restart.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Restart extends Component {
+    constructor(props) {	
+		super(props);
+	}
+	
+	render() {
+		return (
+			<div id='reset'>
+                <span id='undo'>undo</span>
+                <span id='restart'>restart</span>
+            </div>
+		);
+	}
+}
+
+
+export default Restart;
